9a8da564e5d9590d5eae064f812e593060363176
awbral <axel.bral@ugent.be>
2023-06-29 17:31:56 +0200

    add script to perform solely post-processing

---
+++ run_files/run_post_processing.py
@@ -0,0 +1,15 @@
+import json
+from os.path import join, dirname
+from analysis import Analysis
+import sys
+
+# import parameters
+if len(sys.argv) == 2:
+    parameter_file_name = join(dirname(__file__), sys.argv[1])
+else:
+    parameter_file_name = join(dirname(__file__), 'parameters.json')
+with open(parameter_file_name, 'r') as parameter_file:
+    parameters = json.load(parameter_file)
+
+simulation = Analysis(parameters)
+simulation.post_process()
